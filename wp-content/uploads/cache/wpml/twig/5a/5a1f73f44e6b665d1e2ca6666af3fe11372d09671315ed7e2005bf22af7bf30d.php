d88b63b8fbdc4827091aa3ee100b19928b241763
Jorge Sierra <jorgemaikelsierra@outlook.com>
2023-08-05 19:12:58 +0200

    Actualizacion del .gitignore

---
+++ wp-content/uploads/cache/wpml/twig/5a/5a1f73f44e6b665d1e2ca6666af3fe11372d09671315ed7e2005bf22af7bf30d.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace WPML\Core;
+
+use \WPML\Core\Twig\Environment;
+use \WPML\Core\Twig\Error\LoaderError;
+use \WPML\Core\Twig\Error\RuntimeError;
+use \WPML\Core\Twig\Markup;
+use \WPML\Core\Twig\Sandbox\SecurityError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedTagError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFilterError;
+use \WPML\Core\Twig\Sandbox\SecurityNotAllowedFunctionError;
+use \WPML\Core\Twig\Source;
+use \WPML\Core\Twig\Template;
+
+/* dialog-box.twig */
+class __TwigTemplate_3d1e2c5aef76949ad9b4523bc77666b626dd1ffd3e09d663abd634a0dd3af9ff extends \WPML\Core\Twig\Template
+{
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        // line 1
+        echo "<div id=\"wpml-ls-dialog\" style=\"display:none;\" >
+    <div class=\"js-wpml-ls-dialog-inner\">
+
+    </div>
+    <div class=\"wpml-dialog-footer \">
+        <span class=\"errors icl_error_text\"></span>
+        <input class=\"js-wpml-ls-dialog-close cancel wpml-dialog-close-button alignleft\" value=\"";
+        // line 7
+        echo \WPML\Core\twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["strings"] ?? null), "misc", []), "button_cancel", []), "html", null, true);
+        echo "\" type=\"button\">
+        <input class=\"js-wpml-ls-dialog-save button-primary term-save alignright\" value=\"";
+        // line 8
+        echo \WPML\Core\twig_escape_filter($this->env, $this->getAttribute($this->getAttribute(($context["strings"] ?? null), "misc", []), "button_save", []), "html", null, true);
+        echo "\" type=\"submit\">
+        <span class=\"spinner alignright\"></span>
+    </div>
+</div>";
+    }
+
+    public function getTemplateName()
+    {
+        return "dialog-box.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  44 => 8,  40 => 7,  32 => 1,);
+    }
+
+    /** @deprecated since 1.27 (to be removed in 2.0). Use getSourceContext() instead */
+    public function getSource()
+    {
+        @trigger_error('The '.__METHOD__.' method is deprecated since version 1.27 and will be removed in 2.0. Use getSourceContext() instead.', E_USER_DEPRECATED);
+
+        return $this->getSourceContext()->getCode();
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "dialog-box.twig", "/homepages/26/d944497268/htdocs/aransmethod/wp-content/plugins/sitepress-multilingual-cms/templates/language-switcher-admin-ui/dialog-box.twig");
+    }
+}
